84cbbea0dce67809137b92a594a0980d36b82cb6
Jesse Okeya <jesseokeya@Jesses-MacBook-Pro.local>
2016-12-19 23:05:01 -0500

    Bug fix
    Fixed the browser opening functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require("path");
+var open = require('open');
 
 var app = express();
 var router = express.Router();
@@ -16,4 +17,8 @@ router.get('/:user', function(req, res){
 
 app.listen(port, function(){
   console.log('Server live at http://localhost:' + port);
+  open('http://localhost:' + port, function (err) {
+    if (err) throw err;
+    console.log('Browser opening at http://localhost:' + port);
+  });
 });
